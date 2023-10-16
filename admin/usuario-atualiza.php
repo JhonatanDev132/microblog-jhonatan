73b909a432d9f17d3b422fc1abe2a4768b58713c
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-16 10:25:36 -0300

    Update usuario-atualiza.php

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -4,9 +4,19 @@ use Microblog\Utilitarios;
 
 require_once "../inc/cabecalho-admin.php";
 
+/* Script para carregamento */
 $usuario = new Usuario;
 $usuario->setId($_GET["id"]);
 $dados = $usuario->listarUm();
+
+/* Script para atualização */
+if( isset($_POST["atualizar"]) ){
+	$usuario->setNome($_POST['nome']);
+	$usuario->setEmail($_POST['email']);
+	$usuario->setTipo($_POST['tipo']);
+
+	// E aí vem a senha...
+}
 ?>
 
 
