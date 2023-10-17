6af780f39de85ef131f18b2031e9e424c711a8a3
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-17 09:27:27 -0300

    Método login

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -25,4 +25,13 @@ final class ControleDeAcesso {
         }
     }
 
+
+    public function login(int $id, string $nome, string $tipo):void {
+        /* No momento em que ocorre login, criamos variáveis
+        de sessão contendo os dados que queremos monitorar/controlar/usar através da sessão enquanto a pessoa estiver logada. */
+        $_SESSION["id"] = $id;
+        $_SESSION["nome"] = $nome;
+        $_SESSION["tipo"] = $tipo;
+    }
+
 }
