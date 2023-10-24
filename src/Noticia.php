be3cfb965efa3c41c9d1af3fc4e70f1a1ab4c474
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-24 09:46:15 -0300

    Método inserir

--- src/Noticia.php
+++ src/Noticia.php
@@ -28,6 +28,40 @@ final class Noticia {
         $this->conexao = Banco::conecta();
     }
 
+    /* Métodos CRUD */
+    public function inserir():void {
+        $sql = "INSERT INTO noticias(
+            titulo, texto, resumo,
+            imagem, destaque, 
+            usuario_id, categoria_id
+        ) VALUES(
+            :titulo, :texto, :resumo,
+            :imagem, :destaque, 
+            :usuario_id, :categoria_id
+        )";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":titulo", $this->titulo, PDO::PARAM_STR);
+            $consulta->bindValue(":texto", $this->texto, PDO::PARAM_STR);
+            $consulta->bindValue(":resumo", $this->resumo, PDO::PARAM_STR);
+            $consulta->bindValue(":imagem", $this->imagem, PDO::PARAM_STR);
+            $consulta->bindValue(":destaque", $this->destaque, PDO::PARAM_STR);
+            
+            /* Aqui, primeiro chamamos os getters de ID do Usuario e de Categoria,
+            para só depois associar os valores aos parâmetros da consulta SQL.
+            Isso é possível devido à associação entre as Classes. */
+            $consulta->bindValue(":usuario_id", $this->usuario->getId(), PDO::PARAM_INT);
+            $consulta->bindValue(":categoria_id", $this->categoria->getId(), PDO::PARAM_INT);
+        } catch (Exception $erro) {
+            die("Erro ao inserir notícia: " . $erro->getMessage());
+        }
+    }
+
+
+
+
+
 
     public function getId(): int
     {
