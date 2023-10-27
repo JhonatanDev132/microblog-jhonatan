7af1c83e9d4e3995631e7bee92e5a1bbc3021a65
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-10-27 10:02:09 -0300

    .

--- src/Banco.php
+++ src/Banco.php
@@ -6,7 +6,7 @@ abstract class Banco {
     private static string $servidor = "localhost";
     private static string $usuario = "root";
     private static string $senha = "";
-    private static string $banco = "microblog_tiago";
+    private static string $banco = "microblog_jhonatan";
     
     /* Operador ? "nullable typehint" -> PHP +7.1
     Quando usado, indica que a propriedade/atributo
